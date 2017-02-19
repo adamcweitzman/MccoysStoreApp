ec4f2142637f2048590aa632bd3fd787df3ac938
Adam Weitzman <adamcweitzman@gmail.com>
2017-02-19 13:03:56 -0600

    add store class for data mapping

---
+++ src/MccoysStoreApp/Models/Store.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace MccoysStoreApp.Models
+{
+    public class Store
+    {
+        public int StoreNumber { get; set; }
+        public string StoreName { get; set; }
+        public string StoreManagerName { get; set; }
+        public string OpeningTime { get; set; }
+        public string ClosingTime { get; set; }
+    }
+
+}
